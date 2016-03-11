ff26548fdfc96730409e2403ccd44ce2d7cc57cf
iaroslav <iaroslogos@gmail.com>
2016-03-11 13:28:31 +0100

    Restructured the project

---
+++ dataset_parser.py
@@ -0,0 +1,10 @@
+'''
+Parses the dataset consisting of 
+@author: iaroslav
+'''
+
+def read_dataset(name):
+    """
+    It is assumed that the dataset is located in the project folder
+    """
+    
