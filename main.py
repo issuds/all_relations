5a0d0052d3d3c3f6bf1de7bbbd7f771e06f6ea38
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2017-10-24 14:54:45 +0200

    Wiki results, update to gitignore

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as ps
 
 from backend.skapi import pandas_to_concepts, all_1_to_1, all_n_to_1
 
-data = ps.read_csv('datasets/utaut.csv')
+data = ps.read_csv('datasets/wiki.csv')
 data = ps.DataFrame(data, dtype='float')
 data = data.dropna()
 concepts = pandas_to_concepts(data)
