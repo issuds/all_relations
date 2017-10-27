dae5e0f2d138a174d6db86078dbe50e108e242ff
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2017-10-27 11:39:04 +0200

    Add installation scripts

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install python3 python3-pip -y
+pip3 install numpy
+pip3 install scipy
+pip3 install pandas
+pip3 install scikit-learn
+pip3 install scikit-optimize
+pip3 install tqdm
