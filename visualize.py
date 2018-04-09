0d56189e8aff9aff7aea02e78962d551472c7339
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-04-09 17:53:53 +0200

    CSV "visualizations" of the data

--- visualize.py
+++ visualize.py
@@ -5,13 +5,13 @@ import json
 import pydot
 import pandas as pd
 
-fpath = os.path.join('results', 'wiki.json')
+fpath = os.path.join('results', 'gender_discrimination.json')
 
-skip_w = 0.2
+skip_w = -100.0
 
 # optional abbreviations json, which contains descriptions to concept abbreviations
 # set to None if no are available
-abbs = None
+abbs = True
 if abbs:
     abbs = os.path.join('datasets', 'gender_discrimination_workplace.info')
     abbs = json.load(open(abbs, 'r'))
@@ -19,25 +19,26 @@ if abbs:
 relations = json.load(open(fpath, 'r'))
 relations.sort(reverse=True, key=lambda x: x[-1])
 
-dataframe = pd.DataFrame();
+dataframe = pd.DataFrame()
 graph_data = ["digraph {"]
+
 
 for A, B, w in relations:
     for a in A:
         for b in B:
             w = np.round(w, 3)
 
-            # write the relation in the csv
-            dataframe.at[a, b] = w
-
-            if w < skip_w:
-                continue
-
             an, bn = a, b
 
             if abbs:
                 an = abbs[an]
                 bn = abbs[bn]
+
+            # write the relation in the csv
+            dataframe.at[an, bn] = w
+
+            if w < skip_w:
+                continue
 
             line = '"%s" -> "%s"[label="%s"]' % (an, bn, w)
             graph_data.append(line)
@@ -49,4 +50,8 @@ graph_data = "\n".join(graph_data)
 # generate dot file
 (graph,) = pydot.graph_from_dot_data(graph_data)
 graph.write_svg('visualization.svg')
+
+dataframe = dataframe.sort_index(axis=0)
+dataframe = dataframe.sort_index(axis=1)
+
 dataframe.to_csv('visualization.csv')
