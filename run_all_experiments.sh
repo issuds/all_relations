88f06a0f63daede1b86789a1058ac5a2f2411d16
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-10-21 14:40:33 +0200

    update experiments

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -2,7 +2,6 @@ python3 -W ignore main.py lasso --prefix
 python3 -W ignore main.py ann --prefix
 python3 -W ignore main.py knn --prefix
 python3 -W ignore main.py gbrt --prefix
-
 python3 -W ignore main.py lasso
 python3 -W ignore main.py ann
 python3 -W ignore main.py knn
