f55bc45a241623049836f6bc5329c1a64eccb182
Ubuntu <ubuntu@ip-172-31-33-230.ec2.internal>
2018-10-21 13:17:17 +0000

    initial results

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+
 sudo apt-get update
+sudo apt install python-pydot python-pydot-ng graphviz -y
 sudo apt-get install python3 python3-pip -y
+pip3 install pydot
+pip3 install scikit-learn==0.19.2
 pip3 install numpy
 pip3 install scipy
 pip3 install pandas
-pip3 install scikit-learn
-pip3 install scikit-optimize
 pip3 install tqdm
 pip3 install docopt
--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,8 +1,9 @@
-python3 -W ignore main.py lasso --prefix
-python3 -W ignore main.py ann --prefix
-python3 -W ignore main.py knn --prefix
-python3 -W ignore main.py gbrt --prefix
-python3 -W ignore main.py lasso
+#python3 -W ignore main.py lasso
+#python3 -W ignore main.py lasso --prefix
+#python3 -W ignore main.py ann --prefix
+#python3 -W ignore main.py knn --prefix
+#python3 -W ignore main.py gbrt --prefix
+#python3 -W ignore main.py lasso
+#python3 -W ignore main.py knn
+#python3 -W ignore main.py gbrt
 python3 -W ignore main.py ann
-python3 -W ignore main.py knn
-python3 -W ignore main.py gbrt
